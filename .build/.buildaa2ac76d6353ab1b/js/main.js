99e4e2190ddf3d2a3308037c7e0647959170fc42
Arunram Kalaiselvan <arun@edengroups.com>
2013-10-11 21:43:24 -0400

    first commit , project created with jquery ui

---
+++ .build/.buildaa2ac76d6353ab1b/js/main.js
@@ -0,0 +1,12 @@
+//Initialize function
+var init = function () {
+    // TODO:: Do your initialization job
+    console.log("init() called");
+
+    // add eventListener for tizenhwkey
+    document.addEventListener('tizenhwkey', function(e) {
+        if(e.keyName == "back")
+            tizen.application.getCurrentApplication().exit();
+    });
+};
+$(document).ready(init);
