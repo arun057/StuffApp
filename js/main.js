359eb17aea3a68d9bcc0fa613d80a4f94cfdf889
Jacky Alciné <me@jalcine.me>
2013-10-12 15:47:56 -0400

    Updated logic.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
 //Initialize function
 var init = function () {
-    // TODO:: Do your initialization job
-    console.log("init() called");
-
     // add eventListener for tizenhwkey
     document.addEventListener('tizenhwkey', function(e) {
         if(e.keyName == "back")
             tizen.application.getCurrentApplication().exit();
+    });
+    
+    var img = $("#awesome");
+    img.click(function(){
+    	if (img.attr("data-awesome")){
+    		img.attr("src","img/gang-on.png");
+    		img.attr("data-awesome","yerp");
+    	} else {
+    		img.attr("src", "img/gang.png");
+    		img.removeAttr("data-awesome");
+    	}
     });
 };
 $(document).ready(init);
